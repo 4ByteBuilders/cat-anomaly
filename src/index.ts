b1d3b0d90469a08ed4fbd09331f469bb2ee859af
Mau_2993 <shastrimayur2993@gmail.com>
2025-08-29 02:38:33 +0530

    Comment: Commented out random log generation

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { startRawLogProcessor } from "./UsageStatisticsAggregator.js";
 
 async function main() {
   // --- Start the background job ---
-  startLogGeneration();
+  // startLogGeneration(); //comment out when doing actual thing
   startRawLogProcessor();
   startAnomalyDetector();
   // --- Start your main application logic here ---
